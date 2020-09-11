884ed0143db689a7de857779125efe602c6403fb
luiscostalafc <luiscostalafc@gmail.com>
2020-09-10 21:55:39 -0300

    Densidade de pixel

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import { Image } from 'react-native'
 
 import { Container } from './styles'
 
+import logoImg from '../../assets/logo.png'
+
 const SignUp: React.FC = () => {
-  return <Container />
+  return (
+   
+    <Container>
+      <Image source={logoImg} />
+    </Container>
+
+  )
 }
 
 export default SignUp
