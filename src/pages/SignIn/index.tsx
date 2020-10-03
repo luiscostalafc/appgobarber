51225f49edc495c8f5f3b3e3df7d70d9c7c49c9f
luiscostalafc <luiscostalafc@gmail.com>
2020-10-02 21:50:39 -0300

    Tela de cadastro

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,15 +1,29 @@
 import React from 'react'
-import { Image, View, ScrollView, KeyboardAvoidingView, Platform } from 'react-native'
+import {
+   Image,
+   View, 
+   ScrollView, 
+   KeyboardAvoidingView, 
+   Platform } from 'react-native'
 import Icon from 'react-native-vector-icons/Feather'
+import { useNavigation } from '@react-navigation/native'
 
 import Input from '../../components/Input'
 import Button from '../../components/Button'
 
 import logoImg from '../../assets/logo.png'
 
-import { Container, Title, ForgotPassword, ForgotPasswordText, CreateAccountButton, CreateAccountButtonText } from './styles'
+import { 
+  Container,
+  Title,
+  ForgotPassword,
+  ForgotPasswordText,
+  CreateAccountButton,
+  CreateAccountButtonText 
+} from './styles'
 
 const SignIn: React.FC = () => {
+  const navigation = useNavigation()
   return (
 
     <>
@@ -45,7 +59,7 @@ const SignIn: React.FC = () => {
       </KeyboardAvoidingView>
 
 
-      <CreateAccountButton onPress={() => { }}>
+      <CreateAccountButton onPress={() => navigation.navigate('SignUp')}>
         <Icon name="log-in" size={20} color="#ff9000" />
         <CreateAccountButtonText>Criar uma conta</CreateAccountButtonText>
       </CreateAccountButton>
