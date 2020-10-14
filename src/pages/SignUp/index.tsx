32329ca19982549fffa671dd6d01e6f64ff91cba
luiscostalafc <luiscostalafc@gmail.com>
2020-10-13 22:27:43 -0300

    Validação dos formulários

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,5 +1,5 @@
-import React, { useRef } from 'react'
-import { Image, View, ScrollView, KeyboardAvoidingView, Platform, TextInput } from 'react-native'
+import React, { useRef, useCallback } from 'react'
+import { Image, View, ScrollView, KeyboardAvoidingView, Platform, TextInput, Alert } from 'react-native'
 import Icon from 'react-native-vector-icons/Feather'
 
 import Input from '../../components/Input'
@@ -7,10 +7,19 @@ import Button from '../../components/Button'
 import { useNavigation } from '@react-navigation/native'
 import { Form } from '@unform/mobile'
 import { FormHandles } from '@unform/core'
+import * as Yup from 'yup'
+
+import getValidationErrors from '../../utils/getValidationErrors'
 
 import logoImg from '../../assets/logo.png'
 
 import { Container, Title, BackToSignIn, BackToSignInText } from './styles'
+
+interface SignUpFormData {
+  name: string
+  email: string
+  password: string
+}
 
 const SignUp: React.FC = () => {
   const formRef = useRef<FormHandles>(null)
@@ -18,6 +27,40 @@ const SignUp: React.FC = () => {
 
   const emailInputRef = useRef<TextInput>(null)
   const passwordInputRef = useRef<TextInput>(null)
+
+  const handleSignUp = useCallback(async (data: SignUpFormData) => {
+    try {
+      formRef.current?.setErrors({})
+
+      const schema = Yup.object().shape({
+        name: Yup.string().required('Nome obrigatório'),
+        email: Yup.string().required('E-mail obrigatório').email('Digite um e-mail válido'),
+        password: Yup.string().min(6, 'No mínimo 6 dígitos'),
+      })
+      await schema.validate(data, {
+        abortEarly:false,
+      })
+
+      // await api.post('/users', data)
+
+      // history.push('/')
+
+    } catch (err) {
+      if (err instanceof Yup.ValidationError) {
+        const errors = getValidationErrors(err)
+
+      formRef.current?.setErrors(errors)
+
+      return
+      }
+
+      Alert.alert(
+        'Erro no cadastro',
+        'Ocorreu um erro ao fazer o cadastro, tente novamente.',
+      )
+
+    }
+  }, [])
 
   return (
 
@@ -39,7 +82,9 @@ const SignUp: React.FC = () => {
               <Title>Crie sua conta</Title>
             </View>
 
-            <Form ref={formRef} onSubmit={() => {}}>
+            <Form 
+             ref={formRef} 
+             onSubmit={handleSignUp}>
               <Input 
                autoCapitalize="words"
                name="name" 
